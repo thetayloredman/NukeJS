9fa201adf1977a6cfa2856c69ffe3d1a4c7714a7
thetayloredman <nutdriver716@gmail.com>
2021-03-07 19:19:04 -0800

    repo: fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,9 @@ module.exports = {
     },
     plugins: ['@typescript-eslint'],
     rules: {
-        'import/extensions': 'off',
-        'import/no-unresolved': 'off',
         'no-console': 'off',
         'max-len': 'off',
         'no-await-in-loop': 'off',
-        'class-methods-use-this': 'off',
-        'import/no-named-default': 'off'
+        'class-methods-use-this': 'off'
     }
 };
